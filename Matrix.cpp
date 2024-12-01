4f49829229c250c4769cf6e79cee768d78343611
Ivabek33 <tiwanski@interia.pl>
2024-12-01 21:39:49 +0100

    metoda diagonalan_k

--- Matrix.cpp
+++ Matrix.cpp
@@ -22,3 +22,28 @@ Matrix& Matrix::diagonalna(int* t) {
 
     return *this;
 }
+
+Matrix& Matrix::diagonalna_k(int k, int* t) {
+    // Sprawdzenie, czy macierz została zainicjalizowana.
+    if (!data) {
+        cerr << "Pamięć dla macierzy nie została zaalokowana. Najpierw zaalokuj pamięć." << endl;
+        return *this;
+    }
+
+    // Wypełniamy całą macierz zerami.
+    for (int i = 0; i < size * size; ++i) {
+        data[i] = 0;
+    }
+
+    // Ustawianie wartości na przesuniętej przekątnej.
+    for (int i = 0; i < size; ++i) {
+        int j = i + k; // Kolumna obliczana na podstawie przesunięcia.
+
+        // Sprawdzenie, czy indeks kolumny mieści się w zakresie macierzy.
+        if (j >= 0 && j < size) {
+            data[i * size + j] = t[i]; // Wpisanie wartości z tabeli.
+        }
+    }
+
+    return *this;
+}
