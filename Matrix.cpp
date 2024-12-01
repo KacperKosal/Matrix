e887b7103f901ece6a3d8751cc0acb415a1bcbad
Kacper Kosal <kacper.kosal@op.pl>
2024-12-01 21:39:10 +0100

    Dodanie domyśłniego konstruktora

--- Matrix.cpp
+++ Matrix.cpp
@@ -1,1 +1,11 @@
 #include "Matrix.hpp"
+
+/**
+ * @brief Domyślny konstruktor klasy Matrix.
+ *
+ * Ustawia wskaźnik danych na nullptr i rozmiar macierzy na 0.
+ * Nie alokuje pamięci dla macierzy.
+ */
+Matrix::Matrix() : data(nullptr), size(0) {
+    cout << "Domyślny konstruktor wywołany. Macierz nie została zaalokowana." << endl;
+}
