3a94747a2f7279215ed80c44d05f42148a26f40d
kadzior <adriankadziolka18@gmail.com>
2024-12-02 23:51:45 +0100

    metoda operatora plus plus

--- Matrix.cpp
+++ Matrix.cpp
@@ -373,3 +373,15 @@ Matrix& Matrix::losuj(int x) {
 
     return *this;
 }
+Matrix& Matrix::operator++(int) {
+    if (!data) {
+        cerr << "Pamięć dla macierzy nie została zaalokowana. Najpierw zaalokuj pamięć." << endl;
+        return *this;
+    }
+
+    for (int i = 0; i < size * size; ++i) {
+        ++data[i];
+    }
+
+    return *this;
+}
